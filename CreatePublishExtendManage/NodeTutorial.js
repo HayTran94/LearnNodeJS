04ac6ff90f8430249dab69219ea8f9506036fe24
Hay Tran Van <haytranvan@Hays-MacBook-Pro.local>
2018-09-29 17:48:14 +0700

    update

---
+++ CreatePublishExtendManage/NodeTutorial.js
@@ -0,0 +1,17 @@
+var Tutor = require('./Tutorial.js');
+exports.NodeTutorial = function () {
+    console.log("Node Tutorial")
+    function pTutor() {
+        var PTutor = Tutor
+        PTutor.tutorial();
+    };
+};
+
+// var Tutor = require('./Tutorial.js');
+// exports.NodeTutorial = function () {
+//     console.log("Node Tutorial")
+//     this.pTutor = function () {
+//         var PTutor = Tutor
+//         PTutor.tutorial();
+//     }
+// }
